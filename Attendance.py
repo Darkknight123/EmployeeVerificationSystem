1bbb6386c9effc0e05f99201378f13288a52fced
kerryphilip <philipkerry57@gmail.com>
2021-06-23 00:17:32 +0300

    added the entry log function

--- Attendance.py
+++ Attendance.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime
 
 path = 'ImageAttendance'
 images = []
@@ -24,6 +25,20 @@ def findEncodings(images):
     return encodeList
 
 
+def markEntry(name):
+    with open('Entry.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
+
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 
@@ -40,7 +55,21 @@ while True:
     for encodeFace, faceLoc in zip(encodesCurFrame, faceCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        print(faceDis)
+        # print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            # print(name)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0.255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            markEntry(name)
+
+    cv2.imshow('webcam', img)
+    cv2.waitKey(1)
 
 # faceLoc=face_recognition.face_locations(imgElon)[0]
 # encodeElon=face_recognition.face_encodings(imgElon)[0]
