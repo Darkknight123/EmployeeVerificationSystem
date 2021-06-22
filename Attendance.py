bfdf6c266171d6d799caf3088a9870d11e268413
kerryphilip <philipkerry57@gmail.com>
2021-06-22 22:45:55 +0300

    reformated the document

--- Attendance.py
+++ Attendance.py
@@ -3,53 +3,52 @@ import numpy as np
 import face_recognition
 import os
 
-path= 'ImageAttendance'
-images=[]
-classNames =[]
-myList =os.listdir(path)
-print (myList)
+path = 'ImageAttendance'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
 for cl in myList:
-     curImg=cv2.imread(f'{path}/{cl}')
-     images.append(curImg)
-     classNames.append(os.path.splitext(cl)[0])
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
 print(classNames)
+
 
 def findEncodings(images):
     encodeList = []
     for img in images:
-        img =cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        encode=face_recognition.face_encodings(img)[0]
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
 
-encodeListKnown=findEncodings(images)
+
+encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 
 cap = cv2.VideoCapture(0)
 
 while True:
-    success, img =cap.read()
-    imgS =cv2.resize(img,(0,0),None,0.25,0.25)
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    faceCurFrame=face_recognition.face_locations(imgS)
-    encodesCurFrame=face_recognition.face_encodings(imgS,faceCurFrame)
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
 
-    for encodeFace,faceLoc in zip(encodesCurFrame,faceCurFrame):
-        matches=face_recognition.compare_faces(encodeListKnown,encodeFace)
-        faceDis=face_recognition.face_distance(encodeListKnown,encodeFace)
+    for encodeFace, faceLoc in zip(encodesCurFrame, faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
         print(faceDis)
 
+# faceLoc=face_recognition.face_locations(imgElon)[0]
+# encodeElon=face_recognition.face_encodings(imgElon)[0]
+# cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
 
+# faceLocTest=face_recognition.face_locations(imgTest)[0]
+# encodeTest=face_recognition.face_encodings(imgTest)[0]
+# cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
 
-
-#faceLoc=face_recognition.face_locations(imgElon)[0]
-#encodeElon=face_recognition.face_encodings(imgElon)[0]
-#cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
-
-#faceLocTest=face_recognition.face_locations(imgTest)[0]
-#encodeTest=face_recognition.face_encodings(imgTest)[0]
-#cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
-
-#result=face_recognition.compare_faces([encodeElon],encodeTest)
-#faceDis=face_recognition.face_distance([encodeElon],encodeTest)
+# result=face_recognition.compare_faces([encodeElon],encodeTest)
+# faceDis=face_recognition.face_distance([encodeElon],encodeTest)
