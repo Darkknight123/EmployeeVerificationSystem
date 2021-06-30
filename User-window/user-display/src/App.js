f4d7e906a35920bc30974db67ef692ebaeb209ca
kerryphilip <philipkerry57@gmail.com>
2021-06-30 13:01:13 +0300

    started working on the window component with react-js

--- User-window/user-display/src/App.js
+++ User-window/user-display/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import Webcam from'react-webcam';
-import {userRef} from 'react'
+
 
 function App() {
 
   return (
     <div className="App">
       React-webcam
-      <Webcam/>
+     <display/>
     </div>
   );
 }
