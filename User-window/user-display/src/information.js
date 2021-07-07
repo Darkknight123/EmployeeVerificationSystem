0f95990355cf19640714da38430822b3b95f09b2
kerryphilip <philipkerry57@gmail.com>
2021-07-07 11:34:34 +0300

    added the axios library

--- User-window/user-display/src/information.js
+++ User-window/user-display/src/information.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React,{useState,useEffect}from 'react'
+import axios from 'axios'
 
 function information() {
+    const [posts,setPost] 
     return (
         <div>
             
