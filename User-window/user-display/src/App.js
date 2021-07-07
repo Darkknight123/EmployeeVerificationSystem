a6dd1acafab4b177b4fcf222140c32bc8b2384c8
kerryphilip <philipkerry57@gmail.com>
2021-07-07 11:25:43 +0300

    added the video container

--- User-window/user-display/src/App.js
+++ User-window/user-display/src/App.js
@@ -1,10 +1,55 @@
+import React, {useState} from 'react'
 import './App.css';
-import display from './display'
+
 function App() {
+  const[playing,setPlaying]=useState(false);
+      
+  const HEIGHT =500;
+  const WIDTH=500;
+
+  const startVideo=()=>{
+    setPlaying(true)
+    navigator.getUserMedia(
+      {
+        video:true,
+      },
+      (stream)=>{
+        let video = document.getElementsByClassName('app_videofeed')[0];
+        if (video){
+          video.srcObject=stream;
+        }
+      },
+      (err)=>console.error(err)
+    );
+
+      };
+
+  const stopVideo=()=> {
+    setPlaying(false)
+    let video = document.getElementsByClassName('app_videofeed')[0];
+    video.srcObject.getTracks()[0].stop();
+
+      };
 
   return (
-    <div className="display">
-      <display/>
+    <div className="app">
+      <div className="app_container">
+        <video
+          height={HEIGHT}
+          width={WIDTH}
+          muted
+          autoplay
+          className="app_videoFeed">
+        </video>
+      </div>
+      <div className="app_input">
+        {playing?(
+          <button onClick={stopVideo}>Stop</button>
+        ):(
+          <button onClick={startVideo}>Start</button>
+        )}
+      </div>
+     
 
     </div>
   );
