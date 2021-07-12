1c68cc0e12d2c4fddf018a8d9acb83aa7b6fd242
kerryphilip <philipkerry57@gmail.com>
2021-07-12 19:54:30 +0300

    starting a fresh on the login page

--- admin-panel/src/SubmitButton.js
+++ admin-panel/src/SubmitButton.js
@@ -1,16 +1,1 @@
-import React from 'react';
 
-
-class submitButton extends React.Component {
-  render(){
-    return (
-      <div className="submitButton">
-        
-      </div>
-    );
-
-  }
- 
-}
-
-export default submitButton;
