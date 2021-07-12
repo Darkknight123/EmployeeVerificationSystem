2440918bc66ddfe2f9cfe350da4dbefafd21b025
kerryphilip <philipkerry57@gmail.com>
2021-07-12 20:55:14 +0300

    using react hooks

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,1 +1,36 @@
+import React,{useState} from 'react';
+import LoginForm from './components/LoginForm'
+
+function App() {
+  const adminUser={
+    email:"admin@admin.com",
+    password:"admin123"
+  }
+  const [user,setUser]=useState({name:"",email:""});
+  const [error,setError]=useState("");
+
+  const Login =details=>{
+    console.log(details);
+  }
+
+  const Logout=()=>{
+    console.log("Logout");
+  }
+
+  return (
+    <div className='App'>
+     {(user.email !="")?(
+     <div className="welcome">
+        <h2>Welcome, <span>{user.name}</span></h2>
+       <button>Logout</button>
+     </div>
+      
+     ):(
+       <LoginForm/>
+     )}
+    </div>
+  );
+}
+
+export default App
 
