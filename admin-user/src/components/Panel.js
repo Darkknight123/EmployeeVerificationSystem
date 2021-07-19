9efb51d71997ad881a3c005e78199ceb6ddc65b3
kerryphilip <philipkerry57@gmail.com>
2021-07-19 17:52:25 +0300

    corrected the pages form

--- admin-user/src/components/Panel.js
+++ admin-user/src/components/Panel.js
@@ -1,9 +1,9 @@
 import "./panel.css";
 import React, { Component } from 'react'
-import Home from './pages/Home'
-import NewUser from './pages/NewUser'
-import Users from './pages/Users'
-import Logs from './pages/Logs'
+import Home from './Home'
+import NewUsers from './NewUsers'
+import Users from './Users'
+import Logs from './Logs'
 
 
 export class Panel extends Component {
@@ -16,7 +16,7 @@ export class Panel extends Component {
                         <div className="Panel">
                                 <div className="btn-container">
                                         <div onClick={this.props.r(<Home/>)}  className="btn home"><i className="fas fa-home"></i></div>
-                                        <div onClick={this.props.r(<NewUser/>)} className="btn new-user"> <i className="fas fa-user-plus"></i></div>
+                                        <div onClick={this.props.r(<NewUsers/>)} className="btn new-user"> <i className="fas fa-user-plus"></i></div>
                                         <div onClick={this.props.r(<Users/>)} className="btn users"> <i className="fas fa-users"></i></div>
                                         <div onClick={this.props.r(<Logs/>)} className="btn logs"> <i className="fas fa-cogs"></i></div>
                                 </div>
