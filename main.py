198ac0b061e25e9c64c64573481413d98ea340ba
kerryphilip <philipkerry57@gmail.com>
2021-09-01 23:30:39 +0300

    working on the database

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ db = []
 known_path = os.path.join(os.getcwd(), "ImageAttendance/Known_faces/")
 unknown_path = os.path.join(os.getcwd(), "ImageAttendance/Unknown_faces/")
 
-DATABASE_USER = os.environ['Dark_Knight']
-DATABASE_PASSWORD = os.environ['Maggie']
-DATABASE_NAME = os.environ['FaceBase']
-DATABASE_HOST = os.environ['127.0.0.1']
-DATABASE_PORT = os.environ['3000']
+#DATABASE_USER = os.environ['Dark_Knight']
+#DATABASE_PASSWORD = os.environ['Maggie']
+#DATABASE_NAME = os.environ['FaceBase']
+#DATABASE_HOST = os.environ['127.0.0.1']
+#DATABASE_PORT = os.environ['3000']*
 
 
 def DATABASE_CONNECTION():
@@ -229,51 +229,37 @@ def add_employee():
     return jsonify(answer)
 
 
-@app.route("/Login")
-def login():
-    get_data()
-    global db
-    if not db:
-        msg = "ACCESS DENIED \n YOU DO NOT HAVE THE PRIVILEGES TO ACCESS THIS ROOM"
-    else:
-        known_face_encodings = [i[1] for i in db]
-        known_face_names = [i[0] for i in db]
-        face_locations = []
-        face_encodings = []
-        face_names = []
-        video_capture = cv2.VideoCapture(0)
-        ret, frame = video_capture.read()
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-        rgb_small_frame = small_frame[:, :, ::-1]
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-        face_names = []
-        if not face_encodings:
-            msg = "ACCESS DENIED \n YOU DO NOT HAVE THE PRIVILEGES TO ACCESS THIS ROOM"
-        else:
-            for face_encoding in face_encodings:
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
-                if name == "Unknown":
-                    msg = "ACCESS DENIED \n YOU DO NOT HAVE THE PRIVILEGES TO ACCESS THIS ROOM"
-                else:
-                    msg = name
-                face_names.append(name)
-            for (top, right, bottom, left), name in zip(face_locations, face_names):
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-            os.chdir(unknown_path)
-            rand_no = np.random.random_sample()
-            cv2.imwrite(str(rand_no) + ".jpg", frame)
-        return msg
-    if __name__ == '__main__':
-        app.run()
+# *------- Get Employee List -------*
+@app.route('/users', methods=['GET'])
+def users():
+    users = {}
+
+    # get the user list from the user folder
+    walk_count = 0
+    for file_name in os.listdir(f"{FILE_PATH}/ImageAttendance/users/"):
+        # capture the employee's name with the file's name
+        name = re.findall("(.*)\.jpg,file_name")
+        if name:
+            users[walk_count] = name[0]
+            walk_count += 1
+
+    return jsonify(users)
+
+
+# *------- Delete employee -------*
+@app.route('/delete_employee/<string:FirstName>', methods=['GET'])
+def delete_employee(FirstName):
+    try:
+        # Remove the picture of the employee from the user's folder:
+        print('name:', FirstName)
+        file_path = os.path.join(f'ImageAttendance/users{FirstName}.jpg')
+        os.remove(file_path)
+        answer = 'Employee succesfully deleted from the system'
+    except:
+        answer = 'Error while deleting the user. Please try again later....'
+    return jsonify(answer)
+
+
+# *----------- RUN SERVER --------------*
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
